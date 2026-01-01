e18191d4fc6aa38652fdeca034a4a868c66ebec4
joaoDevL <joao.software.dev@gmail.com>
2025-12-31 23:05:00 -0500

    feat: Adding the form component

---
+++ components/ui/form.tsx
@@ -0,0 +1,22 @@
+interface FormProps {
+  children: React.ReactNode;
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+  title?: string;
+  style?: string;
+}
+
+const Form = ({
+  children,
+  onSubmit,
+  title,
+  style = "flex flex-col gap-6 pt-10 pb-10 pr-5 pl-5 m-4 bg-four rounded-lg",
+}: FormProps) => {
+  return (
+    <form onSubmit={onSubmit} className={style}>
+      {title && <h3 className="text-center text-xl">{title}</h3>}
+      {children}
+    </form>
+  );
+};
+
+export default Form;
