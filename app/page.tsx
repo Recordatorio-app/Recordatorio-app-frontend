474ae998b68ee9b79e97381c66187106674ccfae
joaoDevL <joao.software.dev@gmail.com>
2025-12-31 22:50:33 -0500

    feat: Adding the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import Button from "@/components/ui/button";
+import Form from "@/components/ui/form";
+import Input from "@/components/ui/input";
+import { useAuth } from "@/context/AuthContext";
+import { loginUser } from "@/services/auth/auth.service";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const res = await loginUser({ email, password });
+    login(res.token, res.user);
+    if (res.token && res.user) {
+      alert("Inicio de sesión exitoso");
+      setInterval(() => router.push("/home"), 2000); //redirigir a home despues de 2 segundos
+    }
+  };
   return (
     <main className="flex flex-col justify-center items-center min-h-screen bg-five">
-      <section className="mb-10 text-center">
+      <section className="mb-10 text-center mt-10">
         <h1>Bienvenido a tu recordatorio de activades</h1>
       </section>
       <section className="mb-10 w-4/4 text-center">
@@ -10,26 +37,35 @@ export default function Login() {
         </h2>
       </section>
       <section>
-        <form className="flex flex-col gap-6 p-10 m-6 bg-four rounded-lg ">
-          <h3 className="text-center text-xl">Iniciar Sesión</h3>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="Correo" className="text-xs">Correo Electrónico</label>
-            <input type="email" id="Correo" className="bg-white rounded-md p-1 h-8 text-xs focus:outline-none focus:ring-2 focus:ring-one" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="Password" className="text-xs">Contraseña</label>
-            <input type="password" id="Password" className="bg-white rounded-md p-1 h-8 text-xs focus:outline-none focus:ring-2 focus:ring-one" />
+        <Form
+          onSubmit={handleSubmit}
+          title="Iniciar Sesión"
+        >
+          <Input
+            label="Correo Electrónico"
+            placeholder="Correo electrónico"
+            id="Correo"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="flex items-center justify-center gap-2">
+            <Input
+              label="Contraseña"
+              placeholder="Contraseña"
+              id="Password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span>a</span>
           </div>
           <span className="text-center text-xs">
             ¿No tienes una cuenta?{" "}
-            <a href="#" className="text-white">
+            <Link href="/register" className="text-white">
               Registrate aquí
-            </a>
+            </Link>
           </span>
-          <button type="submit" className="bg-one text-white p-2 rounded-md">
-            Ingresar
-          </button>
-        </form>
+          <Button bg="bg-one" textColor="text-white">Iniciar Sesión</Button>
+        </Form>
       </section>
     </main>
   );
