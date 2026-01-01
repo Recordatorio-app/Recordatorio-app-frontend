3b22461abba225f4d7fccc60ad8016b5a4eb3e4f
joaoDevL <joao.software.dev@gmail.com>
2025-12-31 22:54:48 -0500

    feat: Adding context for user tokens

---
+++ context/AuthContext.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  phone?: string;
+};
+
+type AuthContextType = {
+  user: User | null;
+  login: (token: string, user: User) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+  
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(() => {
+    if (typeof window === "undefined") return null;
+
+    const stored = localStorage.getItem("user");
+    return stored ? JSON.parse(stored) : null;
+  });
+
+  const login = (token: string, user: User) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
