2a35557e0b8e9efc7cb94fe1f75d7d6b858b7683
joaoDevL <joao.software.dev@gmail.com>
2025-12-31 23:34:11 -0500

    styles:Adding an eye icon for the password in the input fields

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,16 @@ import Form from "@/components/ui/form";
 import Input from "@/components/ui/input";
 import { useAuth } from "@/context/AuthContext";
 import { loginUser } from "@/services/auth/auth.service";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   const { login } = useAuth();
   const router = useRouter();
 
@@ -28,19 +30,16 @@ export default function Login() {
   };
   return (
     <main className="flex flex-col justify-center items-center min-h-screen bg-five">
-      <section className="mb-10 text-center mt-10">
+      <section className="mb-10 text-center mt-10 w-3/4">
         <h1>Bienvenido a tu recordatorio de activades</h1>
       </section>
-      <section className="mb-10 w-4/4 text-center">
+      <section className="mb-10 w-3/4 text-center">
         <h2>
           Ingresa tus credenciales para comenzar a registrar tus actividades
         </h2>
       </section>
       <section>
-        <Form
-          onSubmit={handleSubmit}
-          title="Iniciar Sesión"
-        >
+        <Form onSubmit={handleSubmit} title="Iniciar Sesión">
           <Input
             label="Correo Electrónico"
             placeholder="Correo electrónico"
@@ -48,15 +47,38 @@ export default function Login() {
             type="email"
             onChange={(e) => setEmail(e.target.value)}
           />
-          <div className="flex items-center justify-center gap-2">
-            <Input
-              label="Contraseña"
-              placeholder="Contraseña"
-              id="Password"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <span>a</span>
+          <div className="flex items-center gap-2">
+            <div className="relative w-full">
+              <Input
+                label="Contraseña"
+                placeholder="Contraseña"
+                id="Password"
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                bg="bg-transparent absolute right-2 top-2/2 -translate-y-2/2"
+                textColor="text-black"
+              >
+                {showPassword ? (
+                  <Image
+                    src="/icons/hide.png"
+                    alt="Ocultar contraseña"
+                    width={20}
+                    height={20}
+                  />
+                ) : (
+                  <Image
+                    src="/icons/show.png"
+                    alt="Mostrar contraseña"
+                    width={20}
+                    height={20}
+                  />
+                )}
+              </Button>
+            </div>
           </div>
           <span className="text-center text-xs">
             ¿No tienes una cuenta?{" "}
@@ -64,7 +86,9 @@ export default function Login() {
               Registrate aquí
             </Link>
           </span>
-          <Button bg="bg-one" textColor="text-white">Iniciar Sesión</Button>
+          <Button bg="bg-one" textColor="text-white">
+            Iniciar Sesión
+          </Button>
         </Form>
       </section>
     </main>
