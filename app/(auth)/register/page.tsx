05081dd45bbd5671340a86966072e9f8bf976c1b
joaoDevL <joao.software.dev@gmail.com>
2025-12-31 22:47:25 -0500

    feat: Adding the user registration page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import Button from "@/components/ui/button";
+import Form from "@/components/ui/form";
+import Input from "@/components/ui/input";
+import { useAuth } from "@/context/AuthContext";
+import { registerUser } from "@/services/auth/auth.service";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    if (password !== confirmPassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
+    e.preventDefault();
+
+    const res = await registerUser({ name, email, phone, password });
+    login(res.token, res.user);
+    if (res.token && res.user) {
+      alert("Registro exitoso");
+    }
+    setInterval(() => router.push("/home"), 2000); //redirigir a home despues de 2 segundos
+  };
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen bg-two">
+      <section className="mb-10 text-center text-white mt-20">
+        <h1>Bienvenido al registro para tu recordatorio de actividades</h1>
+      </section>
+      <section className="mb-10 w-4/4 text-center text-white">
+        <h2>Registrate para comenzar a crear tus recordatorios</h2>
+      </section>
+      <section>
+        <Form onSubmit={handleSubmit} title="Registrarse">
+          <Input
+            label="Nombre Completo"
+            placeholder="Nombre completo"
+            id="Nombre"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div className="flex flex-row gap-2">
+            <Input
+              label="Correo Electrónico"
+              placeholder="Correo electrónico"
+              id="Correo"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              label="Teléfono"
+              placeholder="Número de teléfono"
+              id="Telefono"
+              type="number"
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center justify-center gap-2">
+            <Input
+              label="Contraseña"
+              placeholder="Contraseña"
+              id="Password"
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "O" : "M"}
+            </button>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <Input
+              label="Repetir Contraseña"
+              placeholder="Contraseña"
+              id="newPassword"
+              type={showConfirmPassword ? "text" : "password"}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            >
+              {showConfirmPassword ? "O" : "M"}
+            </button>
+          </div>
+          <span className="text-center text-xs">
+            ¿Ya tienes una cuenta?{" "}
+            <Link href="/" className="text-white">
+              Inicia sesión aquí
+            </Link>
+          </span>
+          <Button bg="bg-one" textColor="text-white">Registrarse</Button>
+        </Form>
+      </section>
+    </main>
+  );
+}
