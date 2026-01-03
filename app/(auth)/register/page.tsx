fa03ad1463ba1853d0b94dfc4358d4304f6bb924
JOAOTIX <joao.software.dev@gmail.com>
2026-01-02 19:11:15 -0500

    refactor: Adding form validations and alerts with the sweetAlert library

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -5,10 +5,13 @@ import Form from "@/components/ui/form";
 import Input from "@/components/ui/input";
 import { useAuth } from "@/context/AuthContext";
 import { registerUser } from "@/services/auth/auth.service";
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import "sweetalert2/src/sweetalert2.scss";
 
 export default function Login() {
   const [name, setName] = useState("");
@@ -18,23 +21,105 @@ export default function Login() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const [errors, setErrors] = useState<{
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+    name?: string;
+    phone?: string;
+    general?: string;
+  }>({});
+  const [loading, setLoading] = useState(false);
   const { login } = useAuth();
   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const validate = () => {
+    const newErrors: typeof errors = {};
+
+    if (!email.trim()) {
+      newErrors.email = "El correo es obligatorio";
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      newErrors.email = "Correo inválido";
+    }
+
+    if (!password.trim()) {
+      newErrors.password = "La contraseña es obligatoria";
+    } else if (password.length < 8) {
+      newErrors.password = "Mínimo 8 caracteres";
+    }
     if (password !== confirmPassword) {
-      alert("Las contraseñas no coinciden");
-      return;
+      newErrors.confirmPassword = "Las contraseñas no coinciden";
     }
 
+    if (!name.trim()) {
+      newErrors.name = "El nombre es obligatorio";
+    }
+
+    if (!phone.trim()) {
+      newErrors.phone = "El teléfono es obligatorio";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!validate()) return;
 
-    const res = await registerUser({ name, email, phone, password });
-    login(res.token, res.user);
-    if (res.token && res.user) {
-      alert("Registro exitoso");
+    try {
+      setLoading(true);
+      setErrors({});
+      const res = await registerUser({
+        name,
+        email,
+        phone: "51" + phone,
+        password,
+      });
+      login(res.token, res.user);
+      if (res.token && res.user) {
+        Swal.fire({
+          title: "¡Registro exitoso!",
+          text: "Has iniciado sesión correctamente.",
+          icon: "success",
+          buttonsStyling: false,
+          customClass: {
+            confirmButton: "bg-one text-white px-4 py-2 rounded-md",
+          },
+        });
+      }
+      setTimeout(() => router.push("/home"), 2000); //redirigir a home despues de 2 segundos
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        setErrors({
+          general: error?.response?.data?.message || "Error en el registro",
+        });
+        Swal.fire({
+          title: "Error",
+          text: error?.response?.data?.message || "Error en el registro",
+          icon: "error",
+          buttonsStyling: false,
+          customClass: {
+            confirmButton: "bg-one text-white px-4 py-2 rounded-md",
+          },
+        });
+      } else {
+        setErrors({
+          general: "Ocurrió un error inesperado",
+        });
+        Swal.fire({
+          title: "Error",
+          text: "Ocurrió un error inesperado",
+          icon: "error",
+          buttonsStyling: false,
+          customClass: {
+            confirmButton: "bg-one text-white px-4 py-2 rounded-md",
+          },
+        });
+      }
+    } finally {
+      setLoading(false);
     }
-    setInterval(() => router.push("/home"), 2000); //redirigir a home despues de 2 segundos
   };
   return (
     <main className="flex flex-col justify-center items-center min-h-screen bg-two">
@@ -46,13 +131,29 @@ export default function Login() {
       </section>
       <section>
         <Form onSubmit={handleSubmit} title="Registrarse">
-          <Input
-            label="Nombre Completo"
-            placeholder="Nombre completo"
-            id="Nombre"
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-          />
+          {errors.general && (
+            <span className="text-red-500 text-s text-center">
+              {errors.general}
+            </span>
+          )}
+          <div className="flex items-center gap-2">
+            <Input
+              label="Nombre Completo"
+              placeholder="Nombre completo"
+              id="Name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              icon={
+                <Image
+                  src="/icons/user.png"
+                  alt="Nombre completo"
+                  width={20}
+                  height={20}
+                />
+              }
+              error={errors.name}
+            />
+          </div>
           <div className="flex flex-row gap-2">
             <Input
               label="Correo Electrónico"
@@ -60,6 +161,15 @@ export default function Login() {
               id="Correo"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
+              icon={
+                <Image
+                  src="/icons/email.png"
+                  alt="Correo electrónico"
+                  width={20}
+                  height={20}
+                />
+              }
+              error={errors.email}
             />
             <Input
               label="Teléfono"
@@ -67,74 +177,73 @@ export default function Login() {
               id="Telefono"
               type="number"
               onChange={(e) => setPhone(e.target.value)}
+              icon={
+                <Image
+                  src="/icons/phone.png"
+                  alt="Número de teléfono"
+                  width={20}
+                  height={20}
+                />
+              }
+              error={errors.phone}
             />
           </div>
 
           <div className="flex items-center gap-2">
-            <div className="relative w-full">
-              <Input
-                label="Contraseña"
-                placeholder="Contraseña"
-                id="Password"
-                type={showPassword ? "text" : "password"}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                bg="bg-transparent absolute right-2 top-2/2 -translate-y-2/2"
-                textColor="text-black"
-              >
-                {showPassword ? (
+            <Input
+              label="Contraseña"
+              placeholder="Contraseña"
+              id="Password"
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              icon={
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
                   <Image
-                    src="/icons/hide.png"
-                    alt="Ocultar contraseña"
-                    width={20}
-                    height={20}
+                    src={showPassword ? "/icons/hide.png" : "/icons/show.png"}
+                    alt={
+                      showPassword ? "Ocultar contraseña" : "Mostrar contraseña"
+                    }
+                    width={25}
+                    height={25}
                   />
-                ) : (
-                  <Image
-                    src="/icons/show.png"
-                    alt="Mostrar contraseña"
-                    width={20}
-                    height={20}
-                  />
-                )}
-              </Button>
-            </div>
+                </button>
+              }
+              error={errors.password}
+            />
           </div>
           <div className="flex items-center gap-2">
-            <div className="relative w-full">
-              <Input
-                label="Confirmar Contraseña"
-                placeholder="Contraseña"
-                id="ConfirmPassword"
-                type={showConfirmPassword ? "text" : "password"}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <Button
-                type="button"
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                bg="bg-transparent absolute right-2 top-2/2 -translate-y-2/2"
-                textColor="text-black"
-              >
-                {showConfirmPassword ? (
+            <Input
+              label="Confirmar Contraseña"
+              placeholder="Confirmar contraseña"
+              id="ConfirmPassword"
+              type={showConfirmPassword ? "text" : "password"}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              icon={
+                <button
+                  type="button"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
                   <Image
-                    src="/icons/hide.png"
-                    alt="Ocultar contraseña"
-                    width={20}
-                    height={20}
+                    src={
+                      showConfirmPassword
+                        ? "/icons/hide.png"
+                        : "/icons/show.png"
+                    }
+                    alt={
+                      showConfirmPassword
+                        ? "Ocultar contraseña"
+                        : "Mostrar contraseña"
+                    }
+                    width={25}
+                    height={25}
                   />
-                ) : (
-                  <Image
-                    src="/icons/show.png"
-                    alt="Mostrar contraseña"
-                    width={20}
-                    height={20}
-                  />
-                )}
-              </Button>
-            </div>
+                </button>
+              }
+              error={errors.confirmPassword}
+            />
           </div>
           <span className="text-center text-xs">
             ¿Ya tienes una cuenta?{" "}
@@ -142,8 +251,8 @@ export default function Login() {
               Inicia sesión aquí
             </Link>
           </span>
-          <Button bg="bg-one" textColor="text-white">
-            Registrarse
+          <Button bg="bg-one" textColor="text-white" loading={loading}>
+            {loading ? "Registrando..." : "Registrarse"}
           </Button>
         </Form>
       </section>
