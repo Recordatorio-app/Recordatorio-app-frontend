7b6cff6c7484f7172874dab6d328d3bdf1c39f60
joaoDevL <joao.software.dev@gmail.com>
2026-01-03 15:14:46 -0500

    refactor: adding a button for notifications

---
+++ components/EnableNotificationsButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Button from "@/components/ui/button";
+import { requestNotificationPermission } from "@/lib/pushNotifications";
+import Image from "next/image";
+
+const EnableNotificationsButton = () => {
+  const enable = async () => {
+    try {
+      const token = await requestNotificationPermission();
+      console.log("🔥 PUSH TOKEN:", token);
+
+      // aquí luego lo mandas a tu backend
+    } catch (err) {
+      console.error("❌ Error push:", err);
+    }
+  };
+
+  return (
+    <Button onClick={enable} bg="bg-one" textColor="text-white">
+      Activar notificaciones <Image src="/icons/notification.png" alt="Bell" width={20} height={20} className="ml-2 inline" />
+    </Button>
+  );
+};
+
+export default EnableNotificationsButton;
