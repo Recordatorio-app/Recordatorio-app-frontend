0729883a5876432591002469159583030b550d33
joaoDevL <joao.software.dev@gmail.com>
2026-01-03 15:15:22 -0500

    feat:Adding the settings for notifications

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA15k7INupWGXxqeaSLB18jI3ziKOpnKQM",
+  authDomain: "recordatorio-app-23d5c.firebaseapp.com",
+  projectId: "recordatorio-app-23d5c",
+  messagingSenderId: "530379786852",
+  appId: "1:530379786852:web:3f17e1e4286d3c2dc56d71",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "./icons/icon.png",
+  });
+});
